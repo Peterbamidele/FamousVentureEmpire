efd69a33f82d15c31d04701bed4de0a0ca7b8d9a
Peterbamidele <peterbamidele@gmail.com>
2021-07-19 16:27:14 +0100

    intinal commit

---
+++ src/main/java/com/example/famousventureempire/FamousVentureEmpireApplication.java
@@ -0,0 +1,13 @@
+package com.example.famousventureempire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamousVentureEmpireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamousVentureEmpireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/famousventureempire/FamousVentureEmpireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.famousventureempire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamousVentureEmpireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
