0258bcd7eddb590fe5cf4e310601d5cf49ec096f
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-07-28 10:14:38 +0100

    refactored

--- src/main/java/com/example/famousventureempire/data/model/ProductDto.java
+++ src/main/java/com/example/famousventureempire/data/model/ProductDto.java
@@ -1,16 +1,18 @@
 package com.example.famousventureempire.data.model;
 
 import lombok.Data;
+import lombok.NonNull;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 @Data
 public class ProductDto {
-
     private String name;
     private String description;
     private BigDecimal price;
     private MultipartFile image;
     private String category;
+
 }
