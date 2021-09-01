fa9ad87186bd29adb9f1e508e4d3918c5a7bb225
Peterbamidele <peterbamidele@gmail.com>
2021-09-01 12:46:33 +0100

    mail implementation

--- src/main/java/com/example/famousventureempire/services/EmailServices/EmailServiceImpl.java
+++ src/main/java/com/example/famousventureempire/services/EmailServices/EmailServiceImpl.java
@@ -2,12 +2,23 @@ package com.example.famousventureempire.services.EmailServices;
 
 import com.example.famousventureempire.data.model.Product;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Slf4j
 @Service
 public class EmailServiceImpl implements EmailService{
+
+    private final JavaMailSender javaMailSender;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+
     @Override
     public void sendMail(List<Product> productList,String PhoneNumber) {
         StringBuilder stringBuilder= new StringBuilder();
@@ -16,7 +27,13 @@ public class EmailServiceImpl implements EmailService{
         for (Product product:productList) {
             stringBuilder.append(product.getName()).append(" ").append(product.getProductQuantity()).append(" ").append(product.getPrice()).append(" ").append(product.getGrandTotal()).append("\n");
         }
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("onabanjotitus01@gmail.com", "onabanjotitus01@gmail.com");
 
+        msg.setSubject("Order List");
+        msg.setText(stringBuilder.toString());
+
+        javaMailSender.send(msg);
         log.info("You have checked out \n--->{}",stringBuilder.toString());
 
     }
